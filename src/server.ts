6f6e9417c175035cf7582b5e1353bee5f54cbacf
Priscilla Carrenho <priscillaonil@gmail.com>
2023-05-20 15:26:50 -0300

    feat: add memories route

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,21 @@ import "dotenv/config";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
+
 import { authRoutes } from "./routes/auth";
 import { memoriesRoutes } from "./routes/memories";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "path";
 
 const app = fastify();
+
+app.register(multipart);
+
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
 
 app.register(cors, {
   origin: true, //todas URLs de front-end poderão acessar nosso back-end
@@ -18,11 +29,12 @@ app.register(jwt, {
 
 app.register(authRoutes);
 app.register(memoriesRoutes);
+app.register(uploadRoutes);
 
 app
   .listen({
     port: 3333,
-    host: "localhost",
+    host: "0.0.0.0",
   })
   .then(() => {
     console.log("HTTP server running on http://localhost:3333");
